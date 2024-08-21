13283935466e71a76239409254d2c5b89d9feb08
Daniel Zastashkov <zast2020@mail.ru>
2024-08-21 11:59:40 +0300

    remove storyboard

---
+++ YouTube/VideosModule/View/TestViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+
+}
